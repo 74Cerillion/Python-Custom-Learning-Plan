cf176b1f2f33072ef7f8c5c47cd6d5ae11c1844b
74Cerillion <jonhotchner@gmail.com>
2026-09-18 14:21:01 -0400

    write parser

--- Day 26/parser.py
+++ Day 26/parser.py
@@ -1,5 +1,31 @@
-{
+test = {
     "symbol": "AAPL",
     "price": 234.56,
     "timestamp": "2026-09-16T09:31:00"
 }
+
+from datetime import datetime
+
+def _validate(ni):
+    if not str(ni["symbol"]):
+        return "Invalid Stock"
+    if not float(ni["price"]):
+        return "Invalid Price"
+    try:
+        ni['timestamp'] = datetime.fromisoformat(ni["timestamp"])
+    except:
+        return "Invalid Timestamp"
+
+    return ni
+
+def format(ni):
+    toFormat = _validate(ni)
+    try:
+        return ("{}: ${}. Price Obtained: {}".format(toFormat["symbol"],
+                                                   toFormat["price"],
+                                                   toFormat["timestamp"]))
+    except:
+        return "Invalid Structure"
+
+output = format(test)
+print(output)
